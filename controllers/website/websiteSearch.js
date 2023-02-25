54a209e4086962231d83a421caea7399d083396d
Dishant Sharma <dishu5570@gmail.com>
2023-02-25 06:43:19 +0530

    top product

--- controllers/website/websiteSearch.js
+++ controllers/website/websiteSearch.js
@@ -7,7 +7,8 @@ export const showAllWebsites = async (req, res) => {
     const allTags = [...new Set(resources.reduce((tags, resource) => tags.concat(resource.tags.map(tag => tag.toLowerCase())), []))];
     const allCategories = [...new Set(resources.reduce((categories, resource) => categories.concat(resource.category.toLowerCase()), []))];
     const today = new Date();
-    const index = today.getTime() % resources.length;
+    const daysSinceEpoch = Math.floor(today.getTime() / (1000 * 60 * 60 * 24));
+    const index = daysSinceEpoch % resources.length;
     const dailyResource = resources.find((_, i) => i === index);
 
     res.json({ resources, allTags, allCategories, dailyResource });
