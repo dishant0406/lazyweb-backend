74d1813ce8c7ed8aef385c1892572ceccc82d41c
Dishant <dishu5570@gmail.com>
2023-12-25 15:48:53 +0530

    Add AWS SES integration for sending emails

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,38 +1,50 @@
+import * as aws from '@aws-sdk/client-ses';
+import dotenv from 'dotenv';
+import { SendEmailCommand } from '@aws-sdk/client-ses';
 
-import nodemailer from "nodemailer";
-import * as aws from "@aws-sdk/client-ses";
-import * as dotenv from 'dotenv'
+dotenv.config();
 
-dotenv.config()
+const ses_access_key = process.env.SES_ACCESS_KEY;
+const ses_secret_key = process.env.SES_SCERET_KEY;
 
-const ses_access_key = process.env.SES_ACCESS_KEY
-const ses_sec_key = process.env.SES_SCERET_KEY
+console.log('SES Access Key:', process.env.SES_ACCESS_KEY);
+console.log('SES Secret Key:', process.env.SES_SECRET_KEY);
+
 
 const ses = new aws.SESClient({
-  region: "us-east-1",
+  region: 'us-east-1',
   credentials: {
     accessKeyId: ses_access_key,
-    secretAccessKey: ses_sec_key,
+    secretAccessKey: ses_secret_key,
   },
 });
 
-const transporter = nodemailer.createTransport({
-  SES: { ses, aws },
-});
-
 export const sendMail = async (to, subject, html) => {
-  const mailOptions = {
-    from: "admin@lazyweb.rocks",
-    html,
-    subject,
-    to,
+  const params = {
+    Destination: {
+      ToAddresses: [to],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Data: html,
+          Charset: 'UTF-8',
+        },
+      },
+      Subject: {
+        Data: subject,
+        Charset: 'UTF-8',
+      },
+    },
+    Source: 'admin@lazyweb.rocks',
   };
+
+  const sendEmailCommand = new SendEmailCommand(params);
+
   try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log(info);
+    const data = await ses.send(sendEmailCommand);
+    console.log('Email sent:', data);
   } catch (error) {
-    console.log(error);
+    console.error('Error:', error);
   }
-
-
-}
+};
